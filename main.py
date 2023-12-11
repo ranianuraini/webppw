037d2482fa893e3a1c0ea8055c9632276b56f1b7
ranianuraini <112038719+ranianuraini@users.noreply.github.com>
2023-12-11 23:26:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ url = "https://raw.githubusercontent.com/ranianuraini/PencarianPenambanganWeb/ma
 df = pd.read_csv(url)
 
 # Preprocess data
-X = df['judul']
-y = df['kategori']
+X = df['Judul']
+y = df['Label']
 
 # Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
